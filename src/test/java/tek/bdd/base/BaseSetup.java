e0fdd7c143b7bb9f59d3efaf499b70b3ac036f43
nasibamehraban <nasibamehraban@gmail.com>
2024-09-24 19:35:05 -0400

    the bdd project

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -9,13 +9,14 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-
 public class BaseSetup {
 
     private static final Logger LOGGER = LogManager.getLogger(BaseSetup.class);
@@ -44,8 +45,7 @@ public class BaseSetup {
             if (isHeadless)
                 options.addArguments("--headless");
             driver=new ChromeDriver(options);
-        }
-        else  if (browserType.equalsIgnoreCase("edge")){
+        } else  if (browserType.equalsIgnoreCase("edge")){
             EdgeOptions options=new EdgeOptions();
             if (isHeadless)options.addArguments("--headless");
             driver=new EdgeDriver(options);
